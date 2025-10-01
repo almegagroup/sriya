6426f59b7f1cefee23be6faa7cce4aac37a381ee
Almega Group <keeperpconsole@gmail.com>
2025-10-02 00:32:23 +0530

    chore: test commit to trigger CI on dev

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,16 @@
 // Minimal wire-up only
 
 const { onRequest } = require("firebase-functions/v2/https");
+const { defineSecret } = require("firebase-functions/params");
+const SUPABASE_URL = defineSecret("SUPABASE_URL");
+const SUPABASE_SERVICE_ROLE = defineSecret("SUPABASE_SERVICE_ROLE");
 const { createApp } = require("./src/app");
 const { REGION } = require("./src/config");
-
 const app = createApp();
-exports.api = onRequest({ region: REGION }, app);
+exports.api = onRequest(
+  { region: REGION, secrets: [SUPABASE_URL, SUPABASE_SERVICE_ROLE] },
+  app
+);
 const appDev = createApp();
 exports.apiDev = onRequest({ region: REGION }, appDev);
+// test commit 
